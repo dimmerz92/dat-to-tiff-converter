62a01adc47f69ea80885ad2b71dcba839cc86746
dimmerz92 <andrew.weymes@outlook.com>
2023-04-22 21:04:31 +0800

    add a combined panel figure

--- datToTif.py
+++ datToTif.py
@@ -1,11 +1,20 @@
+import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 import sys
 import os
 import re
 
 files = os.listdir() # list all files in the current working directory
 files = [file for file in files if re.match(".*.dat$", file)] # exclude files that are not .dat files
+titles = [re.search("((?=[A-Za-z])(?:(?!_SM|_CELL).)*)", title).group(1) for title in files] # extract titles from filenames
+fig_title = [*set([re.search("((?=CELL)(?:(?!_[A-Z]).)*)", title).group(1) for title in files])][0].replace("_", " ")
+
+# sort in desired lexicographic order
+titles = sorted(titles, key=lambda x:(1,x) if ":" in x else (0,x))
+order={v:i for i, v in enumerate(titles)}
+files = sorted(files, key=lambda x:order[re.search("((?=[A-Za-z])(?:(?!_SM|_CELL).)*)", x).group(1)])
 
 # make an output folder to store images
 try:
@@ -13,10 +22,13 @@ try:
 except Exception as e:
        sys.exit(e)
 
+images = []
+
 # generate and colour each image
-for file in files:
+for i, file in enumerate(files):
         with open(file, "r") as f:
                 img = np.loadtxt(f)
+                images.append(img)
     # Determine the contrast min and max based on file name
         if '1705-1760' in file:
                 contrast_min, contrast_max = 0.05, 0.5
@@ -35,12 +47,39 @@ for file in files:
         else:
                 contrast_min, contrast_max = 0, 0.15
 
-        # Save figure as is
+        # Save individual figure
         fig, ax = plt.subplots()
         image = ax.imshow(img, interpolation="bilinear", cmap="jet", vmin=contrast_min, vmax=contrast_max)
         plt.colorbar(image, ax=ax)
         plt.axis("off")
         plt.tight_layout()
-        plt.savefig(f"Processed{os.sep}{file[:-4]}.tiff_vmin={contrast_min}_vmax={contrast_max}_jet.tiff", dpi=300)
+        plt.savefig(f"processed{os.sep}{file[:-4]}.tiff_vmin={contrast_min}_vmax={contrast_max}_jet.tiff", dpi=300)
+        plt.close(fig)
 
         print(f"Processed file: {file}")
+
+
+# build the combined figure
+ncols = 6
+nrows = math.ceil(len(files) / ncols)
+
+
+fig = plt.figure(figsize=(10,6))
+gs = gridspec.GridSpec(nrows=nrows+1, ncols=ncols, height_ratios=[0.02] + [1]*(nrows))
+
+# Add column titles
+col_titles = np.tile(["Area Under Curve", "Normalised"], ncols//2)
+for i, title in enumerate(col_titles):
+        ax = plt.subplot(gs[i])
+        ax.axis("off")
+        ax.set_title(title, fontweight="bold")
+
+for i, img in enumerate(images):
+        ax = plt.subplot(gs[i + ncols])
+        ax.imshow(img, interpolation="bilinear", cmap="jet", vmin=contrast_min, vmax=contrast_max)
+        ax.set_title(titles[i])
+        ax.set_axis_off()
+
+plt.suptitle(fig_title, fontweight="bold", fontsize="x-large")
+plt.tight_layout()
+fig.savefig(f"{fig_title}.tiff")
